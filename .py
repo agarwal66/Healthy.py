92209c3977748998359c05c9585ff2d3585f02f9
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2022-12-10 13:52:49 +0000

    Initial commit

---
+++ .py
@@ -0,0 +1,85 @@
+
+#GeneratorExit
+#def searcher():
+  #import time
+  #book = "This is a book on prateek book"
+  #time.sleep(4)
+  #while True:
+   # text = (yield)
+    #if text in book:
+     # print("Your text is in the book")
+    #else:
+      #print("Text is not n book")
+#search = searcher()
+#next(search)
+#search.send("prateek send")
+      
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def divide(a, b):
+    #try:
+       # print(f'{a}/{b} is {a / b}')
+    #except ZeroDivisionError as e:
+        #print(e)
+   # else:
+       # print("No Exception")
+#divide(1, 2)
+
+
+
+
+#try
+
+#try:
+  #f = open("does.txt")
+#except Exception as e:
+ # print(e)
+#finally: 
+  #print("Run this anyway----")
+ # f1.close()
+      
+#print("Important stuff")
+
+
+
+
+
+
+#import time
+
+#def some_work(n):
+ # time.sleep(n)
+ # return n
+#if __name__ == ' __main__ ':
+    #print("Now running some work")
+    #some_work(3)
+   # print("done")
+   # some_work(3)
+   # print("called again")
